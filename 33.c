1b4f8825cf7ab0ae66dffd35ddeb64a05cd4b72c
malekhamza054-debug <malekhamza054@gmail.com>
2025-09-24 21:59:34 +0530

    33.c

---
+++ 33.c
@@ -0,0 +1,11 @@
+//Bitwise O.p.
+#include <stdio.h>
+int main() 
+{
+    int a = 45, b = 23;
+    
+    printf("a & b: %d\n", a & b);
+    printf("a | b: %d\n", a | b);
+    printf("a ^ b: %d\n", a ^ b);
+
+}
