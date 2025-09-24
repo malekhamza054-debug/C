281ccede03e4aa626888f23d53e50a4d8ec4b037
malekhamza054-debug <malekhamza054@gmail.com>
2025-09-24 19:27:53 +0530

    14.c

---
+++ 14.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int sq ,cube ,a;
+    printf("\n Enter the value of a ");
+    scanf("%d", &a);
+    sq=a*a;
+    cube=a*a*a;
+    printf("\nSquare of a no.=%d", sq);
+    printf("\nCube of a no.=%d", cube);
+    
+}
