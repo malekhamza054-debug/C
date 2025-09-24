3a1df604afa19f7f88c385c2ff7fb638d23caa07
malekhamza054-debug <malekhamza054@gmail.com>
2025-09-24 21:54:30 +0530

    31.c

---
+++ 31.c
@@ -0,0 +1,12 @@
+//Unary O.p.
+#include <stdio.h>
+int main()
+{
+    int a = 5;
+    
+    printf("a++: %d\n", a++);
+    printf("++a: %d\n", ++a);
+    printf("a--: %d\n", a--);
+    printf("--a: %d\n", --a);
+
+}
