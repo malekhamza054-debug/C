fb6018929aaabf1f585c0be2450611d36603c3ce
malekhamza054-debug <malekhamza054@gmail.com>
2025-09-24 19:38:29 +0530

    16.c

---
+++ 16.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int A=10 ,B=20 ,C;
+    printf("Before Swapping A=%d, B=%d", A,B);
+    
+    C=A;
+    A=B;
+    B=C;
+    
+    printf("\nAfter Swapping A=%d, B=%d", A, B);
+   
+    }
