ba710e6bde8dc3c01aca05b86f76b5a61daaaddd
malekhamza054-debug <malekhamza054@gmail.com>
2025-09-24 19:10:19 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int a, b, sum:
+printf("\n Enter the value of a & b");
+  scanf("%d%d", &a, &b);
+  sum= a+b;
+  printf("Sum of two integers is %d", &sum)
+  
+}
