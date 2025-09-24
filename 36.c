44ddc82217059aa8b849dbba5878bac19f013349
malekhamza054-debug <malekhamza054@gmail.com>
2025-09-24 22:05:32 +0530

    36.c

---
+++ 36.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter the valu of a&b");
+    scanf("%d%d", &a, &b);
+    if (a>b){
+        printf("a is max");
+    }
+    else{
+        printf("b is max");
+    }
+}
