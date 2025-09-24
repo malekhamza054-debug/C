bbf32c38dd094e786b8ef025c3e8656ae7f49a6f
malekhamza054-debug <malekhamza054@gmail.com>
2025-09-24 20:34:31 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+    int a = 15, b = 5;
+     printf("\nAdd: %d", a + b);
+    
+    printf("\nSubtract: %d", a - b);
+    
+    printf("\nMultiply: %d", a * b);
+    
+    printf("\nDivide: %d", a / b);
+
+}
