b2f7ec0e1ab2e2a8760ff8f82f30827917eb9a08
malekhamza054-debug <malekhamza054@gmail.com>
2025-09-24 21:51:45 +0530

    30.c

---
+++ 30.c
@@ -0,0 +1,11 @@
+//Logical O.p.(1or0)output
+#include <stdio.h>
+
+int main() 
+{
+   int a=5, b=10;
+   printf("\n%d", (a<10&&b>5));
+ printf("\n%d", (a<3||b>6));
+  printf("\n%d", !(a<10==b>5));
+ 
+}
