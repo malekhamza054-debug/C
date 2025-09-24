325e8d1ff3fdfbd9a4a349d81fd11d85156c03a0
malekhamza054-debug <malekhamza054@gmail.com>
2025-09-24 19:53:38 +0530

    18.c

---
+++ 18.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float A ,B ,C ,D ,E ,Avg ;
+   printf("Enter any five numbers ");
+   scanf("%f%f%f%f%f", &A,&B, &C, &D, &E );
+   
+    Avg= A+B+C+D+E/5;
+
+
+printf("\nAvg. of five numbers =%f ", Avg);
+
+ }
