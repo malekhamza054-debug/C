4b64a353b455630f13977325814177699f252a7f
malekhamza054-debug <malekhamza054@gmail.com>
2025-09-24 21:57:57 +0530

    32.c

---
+++ 32.c
@@ -0,0 +1,11 @@
+//Relational O.p.
+#include <stdio.h>
+int main() 
+{
+    int a = 10, b = 20;
+    
+    printf("a == b: %d\n", a == b);
+    printf("a != b: %d\n", a != b);
+    printf("a > b: %d\n", a > b);
+
+}
