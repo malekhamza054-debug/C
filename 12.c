53bec0636dec64d84fe1e6bf8042d61271740777
malekhamza054-debug <malekhamza054@gmail.com>
2025-09-24 19:17:08 +0530

    Update 12.c

--- 12.c
+++ 12.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int a, b, product;
+    float a, b, product;
     printf("Enter the value of a&b");
-    scanf("%d%d", &a, &b);
+    scanf("%f%f", &a, &b);
     product= a*b;
-    printf("product of two numbers is %d", product);
+    printf("product of two numbers is %f", product);
 }
